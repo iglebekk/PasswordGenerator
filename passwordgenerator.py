20621d5f705a296ebb9d4da785d52687c8927ca3
Anders Iglebekk <andersiglebekk@AIMBPR15.local>
2020-08-27 08:47:11 +0200

    done

---
+++ passwordgenerator.py
@@ -0,0 +1,23 @@
+import random
+import os
+import pyperclip
+
+chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%&'
+
+os.system('clear')
+print('Number of passwords?')
+number = int(input('[1]') or '1')
+
+
+print('Password length?')
+length = int(input('[20]') or '20')
+
+print('---')
+for p in range(number):
+    password = ''
+    for c in range(length):
+        password += random.choice(chars)
+    pyperclip.copy(password)
+    print(password)
+print('---')
+print('The last password is sent to your clipboard')
